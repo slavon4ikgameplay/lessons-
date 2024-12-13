665a946bbcfe16158caab6302d7508ff4924ffca
Slavik <slavongameplay@gmail.com>
2024-12-13 22:03:22 +0200

    added home work 3.3

--- hw_3.3.py
+++ hw_3.3.py
@@ -1,7 +1,14 @@
 lst = [1 , 2 , 3 , 4 , 5 , 6]
 new_lst = []
 
-if len(lst) % 2 == 0: # якщо список має парну кількість елементів
+if len(lst) == 0: # якщо список порожній
+    part1 = []
+    part2 = []
+    new_lst.append(part1)
+    new_lst.append(part2)
+    print(new_lst)
+
+elif len(lst) % 2 == 0: # якщо список має парну кількість елементів
     part1 = lst[0 : len(lst) // 2]
     part2 = lst[len(lst) // 2 : len(lst)]
     new_lst.append(part1)
@@ -15,9 +22,3 @@ elif len(lst) % 2 != 0: # якщо список має непарну кільк
     new_lst.append(part2)
     print(new_lst)
 
-elif len(lst) == 0: # якщо список порожній
-    part1 = []
-    part2 = []
-    new_lst.append(part1)
-    new_lst.append(part2)
-    print(new_lst)
