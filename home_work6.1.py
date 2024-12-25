28845584dff61a82015acfe314303f69fa9f8ee5
Slavik <slavongameplay@gmail.com>
2024-12-25 02:58:59 +0200

    update home work 6.1

--- home_work6.1.py
+++ home_work6.1.py
@@ -2,12 +2,14 @@ import string
 from string import ascii_letters
 
 your_range = input("Enter the range in which to display the letters in format 'a-b': ")
+if len(your_range) != 3 or your_range[1] != '-' or your_range[0] not in string.ascii_letters or your_range[2] not in string.ascii_letters:
+    print("Invalid format. ")
+else:
+    first_letter = your_range[0]
+    second_letter = your_range[2]
 
-first_letter = your_range[0]
-second_letter = your_range[2]
-
-first_letter_index = string.ascii_letters.index(first_letter)
-second_letter_index = string.ascii_letters.index(second_letter)
+    first_letter_index = string.ascii_letters.index(first_letter)
+    second_letter_index = string.ascii_letters.index(second_letter)
 
 
-print("".join(string.ascii_letters[first_letter_index:second_letter_index+1]))
+    print("".join(string.ascii_letters[first_letter_index:second_letter_index+1]))
