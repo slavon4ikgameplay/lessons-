6301f1bb9f01e141a7cf09fd6e6c80ca3e194e13
Slavik <slavongameplay@gmail.com>
2025-01-04 00:41:37 +0200

    added home work 8.1

---
+++ hw_8.1.py
@@ -0,0 +1,15 @@
+def add_one(some_list):
+    new_str = ''
+    for digit in some_list:
+        new_str += str(digit)
+
+    num = int(new_str) + 1
+
+    result = [int(digit) for digit in str(num)]
+    return result
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
