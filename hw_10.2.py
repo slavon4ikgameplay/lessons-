fcb22588262adb37eba58d2b18d7838c21b6b716
Slavik <slavongameplay@gmail.com>
2025-01-10 21:41:43 +0200

    added home work 10.2

---
+++ hw_10.2.py
@@ -0,0 +1,18 @@
+def first_word(text):
+    """ Пошук першого слова без використання регулярних виразів """
+    text = text.lstrip(" .,")
+    word = ""
+    for char in text:
+        if char.isalpha() or char == "'":
+            word += char
+        else:
+            break
+    return word
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
