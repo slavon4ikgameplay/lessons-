08448470b1bc0b834bf898d58883db9fdbb1e47b
Slavik <slavongameplay@gmail.com>
2025-01-14 22:03:35 +0200

    added home work 11.1

---
+++ 11.2.py
@@ -0,0 +1,16 @@
+def generate_cube_numbers(end):
+    for i in range(2 , end):
+        cube = i**3
+        if cube > end:
+            return
+        yield cube
+
+from inspect import isgenerator
+
+print(list(generate_cube_numbers(1000)))
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print("OK")
